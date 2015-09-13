d7bcb9334cb5f21ee2e603a3a5ed45f6274f11c9
Shuhel Ahmed <shuhel@ymail.com>
2015-09-13 21:47:38 +0600

    added runtime

--- plugin/handler.js
+++ plugin/handler.js
@@ -6,7 +6,12 @@ Plugin.registerSourceHandler('babel.js', compile);
 function compile(compileStep){
 
  var content = compileStep.read().toString('utf-8');
- var result = babel.transform(content, {modules: "system"}).code;
+ var result = babel.transform(content, {
+   modules: "system", optional : [
+   "es7.classProperties",
+   "es7.decorators",
+   "runtime"
+ ]}).code;
 
  var moduleName = compileStep.inputPath.replace(/\.babel\.js$/, '').replace(/\\/g, '/');
  var path = moduleName + '.js';
